f9f4a90c1c4be76d4adb121e70ba34e9c8088e7f
Mauricio <mauba22@outlook.com>
2024-11-07 11:17:36 -0600

    Cambios en el diseño y corrección de un error al generar las imágenes

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
-const generateButton = document.getElementById('generate-btn'), imagesList = document.querySelector('.generated-images-list'),
-    userDescription = document.getElementById('desc')
-
-const imageLimit = 6, imagesUrlList = []
-let selectedImageNumber = null
 const apiKey = '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', apiUrl = 'https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5'
+const generateButton = document.getElementById('generate-btn'), imagesList = document.querySelector('.generated-images-list'),
+    userDescription = document.getElementById('desc'), alertText = document.querySelector('.alert'), limit = 3, imagesUrlList = []
+let selectedImageNumber = null
 
-window.onload = () => {
-    userDescription.value = ''
+window.onload = () => { 
+    userDescription.value = 'ton 618, nasa, space, black hole'
+    generateImages(userDescription.value) 
 }
 
 /* Genera un número aleatorio (1 - 10000) cada que se genera una imagen */
@@ -18,21 +17,21 @@ const generateRandomNumber = (min, max) => {
 const downloadSelectedImg = (url, number) => {
     const imageUrl = document.createElement('a')
     imageUrl.href = url
-    imageUrl.download = `generated image ${number + 1}.jpg`
+    imageUrl.download = `Imagen #${number + 1}.jpg`
     imageUrl.click()
 }
 
 /* Función que genera las imágenes de acuerdo a la descripción */
 async function generateImages(text) {
     imagesList.innerHTML = ''
-    generateButton.disabled = true, generateButton.textContent = 'Generando imágenes, por favor espere...', userDescription.disabled = true
+    generateButton.disabled = true, generateButton.textContent = 'Generando imágenes, por favor espera...', userDescription.disabled = true
     const listText = document.createElement('span')
     listText.textContent = 'Da clic sobre cualquier imagen para descargarla \u{f019}'
    
-    for (let i = 0; i < imageLimit; i++) {
+    for (let i = 0; i < limit; i++) {
+        //* Al agregar el número aleatorio permite que se muestren diferentes resultados cada que se vuelve a consultar
         const randomNum = generateRandomNumber(1, 10000)
         const description = `${text} ${randomNum}`     
-        /* Al agregar el número aleatorio permite que se muestren diferentes resultados cada que se vuelve a consultar */
         const apiResponse = await fetch(apiUrl, {
                 method: "POST",
                 headers: {
@@ -42,24 +41,22 @@ async function generateImages(text) {
                 body: JSON.stringify({ inputs: description }),
             }
         );
-        apiResponse.ok ? imagesList.prepend(listText) : alert('Error al generar la imagen no. ' + (i + 1))     
+        alertText.textContent = apiResponse.ok ? '' : `\u{f071} Ocurrió un error inesperado al generar algunas imágenes \u{f071}`
 
         const blob = await apiResponse.blob()
         const url = URL.createObjectURL(blob)
         imagesUrlList.push(url)
 
         const imagesContainer = document.createElement('div'), imgElement = document.createElement('img')
-        imgElement.src = url, imgElement.alt = `img ${i + 1}`
+        imgElement.src = url
         imagesContainer.classList.add('images-list-container')
         imagesContainer.appendChild(imgElement)
-        imagesList.appendChild(imagesContainer)
+        imagesList.append(imagesContainer)
+        imagesList.appendChild(listText)
 
-        imgElement.addEventListener('click', () => {
-            downloadSelectedImg(url, i)
-        })
+        imgElement.addEventListener('click', () => { downloadSelectedImg(url, i) })
     }
-    generateButton.disabled = false, generateButton.textContent = 'Generar imágenes'
-    userDescription.disabled = false, userDescription.focus()
+    generateButton.disabled = false, generateButton.textContent = 'Generar imágenes', userDescription.disabled = false, userDescription.focus(),
     selectedImageNumber = null
 }
 
